72ad5180434dea4e31d48410bd18c3a8379f7ffc
Jean Michel-Lemieux <jlemieux>
2003-06-23 02:33:10 +0000

    first pass at performing refrsh in background

--- bundles/org.eclipse.team.ui/src/org/eclipse/team/internal/ui/sync/actions/RefreshAction.java
+++ bundles/org.eclipse.team.ui/src/org/eclipse/team/internal/ui/sync/actions/RefreshAction.java
@@ -6,14 +6,15 @@
  */
 package org.eclipse.team.internal.ui.sync.actions;
 
-import java.lang.reflect.InvocationTargetException;
-
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.action.Action;
-import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.team.core.TeamException;
+import org.eclipse.team.core.subscribers.TeamSubscriber;
 import org.eclipse.team.internal.core.Policy;
 import org.eclipse.team.internal.ui.TeamUIPlugin;
 import org.eclipse.team.internal.ui.actions.TeamAction;
@@ -38,33 +39,42 @@ class RefreshAction extends Action {
 	
 	public void run() {
 		final SyncViewer view = actions.getSyncView();
-		view.run(new IRunnableWithProgress() {
-			public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+		try {
+			ActionContext context = actions.getContext();
+			if(context != null) {
+				getResources(context.getSelection());
+				SubscriberInput input = (SubscriberInput)context.getInput();
+				IResource[] resources = getResources(context.getSelection());
+				if (refreshAll || resources.length == 0) {
+					// If no resources are selected, refresh all the subscriber roots
+					resources = input.roots();
+				}
+				scheduleRefresh(input.getSubscriber(), resources);			
+			}
+		} catch(TeamException e) {
+			TeamUIPlugin.handle(e);
+		}
+	}
+	
+	private IResource[] getResources(ISelection selection) {
+		if(selection == null) {
+			return new IResource[0];
+		}
+		return (IResource[])TeamAction.getSelectedAdaptables(selection, IResource.class);					
+	}
+	
+	// concurreny: is anonymous class reference to containing classe's members safe? Are they copied?
+	private void scheduleRefresh(final TeamSubscriber subscriber, final IResource[] resources) {
+		Job job = new Job() {
+			public IStatus run(IProgressMonitor monitor) {
 				try {
-					monitor.beginTask(null, 100);
-					ActionContext context = actions.getContext();
-					if(context != null) {
-						getResources(context.getSelection());
-						SubscriberInput input = (SubscriberInput)context.getInput();
-						IResource[] resources = getResources(context.getSelection());
-						if (refreshAll || resources.length == 0) {
-							// If no resources are selected, refresh all the subscriber roots
-							resources = input.roots();
-						}
-						input.getSubscriber().refresh(resources, IResource.DEPTH_INFINITE, Policy.subMonitorFor(monitor, 100));
-					}
+					subscriber.refresh(resources, IResource.DEPTH_INFINITE, Policy.subMonitorFor(monitor, 100));
 				} catch (TeamException e) {
-					throw new InvocationTargetException(e);
-				} finally {
-					monitor.done();
-				}
+					return e.getStatus();
+				}				
+				return Status.OK_STATUS;
 			}
-			private IResource[] getResources(ISelection selection) {
-				if(selection == null) {
-					return new IResource[0];
-				}
-				return (IResource[])TeamAction.getSelectedAdaptables(selection, IResource.class);					
-			}
-		});
+		};
+		job.schedule();
 	}
 }
