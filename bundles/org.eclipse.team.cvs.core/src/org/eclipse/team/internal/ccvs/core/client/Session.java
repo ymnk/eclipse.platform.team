d8483f3e4e2d102366ff5c9156d48e2700fc02bd
Michael Valenta <mvalenta>
2003-08-18 14:42:30 +0000

    *** empty log message ***

--- bundles/org.eclipse.team.cvs.core/src/org/eclipse/team/internal/ccvs/core/client/Session.java
+++ bundles/org.eclipse.team.cvs.core/src/org/eclipse/team/internal/ccvs/core/client/Session.java
@@ -45,7 +45,6 @@ import org.eclipse.team.internal.ccvs.core.ICVSFolder;
 import org.eclipse.team.internal.ccvs.core.ICVSRepositoryLocation;
 import org.eclipse.team.internal.ccvs.core.ICVSResource;
 import org.eclipse.team.internal.ccvs.core.ICVSResourceVisitor;
-import org.eclipse.team.internal.ccvs.core.ICVSRunnable;
 import org.eclipse.team.internal.ccvs.core.Policy;
 import org.eclipse.team.internal.ccvs.core.client.Command.GlobalOption;
 import org.eclipse.team.internal.ccvs.core.client.Command.QuietOption;
@@ -93,9 +92,6 @@ public class Session {
 
 	private static final boolean IS_CRLF_PLATFORM = Arrays.equals(
 		System.getProperty("line.separator").getBytes(), new byte[] { '\r', '\n' }); //$NON-NLS-1$
-
-	private static Map currentOpenSessions = null;
-	private static final int MAX_SESSIONS_OPEN = 10;
 	
 	private CVSRepositoryLocation location;
 	private ICVSFolder localRoot;
@@ -139,131 +135,6 @@ public class Session {
 		this.location = (CVSRepositoryLocation) location;
 		this.localRoot = localRoot;
 		this.outputToConsole = outputToConsole;
-	}
-	
-	/**
-	 * Execute the given runnable in a context that has access to an open session
-	 * 
-	 * A session will be opened for the provided root. If the root is null, no session is opened.
-	 * However, sessions will be open for nested calls to run and these sessions will not be closed
-	 * until the outer most run finishes.
-	 * 
-	 * @deprecated
-	 */
-	public static void run(final ICVSRepositoryLocation location, final ICVSFolder root, final boolean outputToConsole,
-		final ICVSRunnable runnable, final IProgressMonitor monitor) throws CVSException {
-			
-		// Serialize runnables to allow only one thread to run at a time. 
-		// TODO: The following removed due to bug 40129
-//		CVSWorkspaceRoot.getCVSFolderFor(ResourcesPlugin.getWorkspace().getRoot()).run(new ICVSRunnable() {
-//			public void run(IProgressMonitor monitor) throws CVSException {
-				internalRun(location, root, outputToConsole, runnable, monitor);
-//			}
-//		}, monitor);
-	}
-		
-	private static void internalRun(ICVSRepositoryLocation location, ICVSFolder root, boolean outputToConsole,
-			ICVSRunnable runnable, IProgressMonitor monitor) throws CVSException {
-
-		// Determine if we are nested or not
-		boolean isOuterRun = false;
-		if (currentOpenSessions == null) {
-			// Initialize the variable to be a map
-			currentOpenSessions = new HashMap();
-			isOuterRun = true;
-		}
-		
-		try {
-			monitor = Policy.monitorFor(monitor);
-			monitor.beginTask(null, 100);
-			if (root == null) {
-				// We don't have a root, so just run the runnable
-				// (assuming that nested runs will create sessions)
-				runnable.run(Policy.subMonitorFor(monitor, 90));
-			} else {
-				// Open a session on the root as far up the chain as possible
-				ICVSFolder actualRoot = root;
-				while (actualRoot.isManaged()) {
-					actualRoot = actualRoot.getParent();
-				}
-				// Look for the root in the current open sessions
-				Session session = (Session)currentOpenSessions.get(actualRoot);
-				if (session == null) {
-					// Make sure we don't exceed our maximum
-					if (currentOpenSessions.size() == MAX_SESSIONS_OPEN) {
-						// Pick one at random and close it
-						Object key = currentOpenSessions.keySet().iterator().next();
-						try {
-							((Session)currentOpenSessions.get(key)).close();
-						} catch (CVSException e) {
-							CVSProviderPlugin.log(e);
-						} finally {
-							currentOpenSessions.remove(key);
-						}
-					}
-					// If it's not there, open a session for the given root and remember it
-					session = new Session(location, actualRoot, outputToConsole);
-					session.open(Policy.subMonitorFor(monitor, 10));
-					currentOpenSessions.put(actualRoot, session);
-				}
-				
-				try {
-					root.run(runnable, Policy.subMonitorFor(monitor, 100));
-				} catch (CVSException e) {
-					// The run didn't succeed so close the session and forget it ever existed
-					try {
-						// The session may have been close by a nested run
-						if (currentOpenSessions.get(actualRoot) != null) session.close();
-					} finally {
-						currentOpenSessions.remove(actualRoot);
-						throw e;
-					}
-				}
-			}
-		} finally {
-			monitor.done();
-			if (isOuterRun) {
-				// Close all open sessions
-				try {
-					Iterator iter = currentOpenSessions.keySet().iterator();
-					while (iter.hasNext()) {
-						Session session = (Session)currentOpenSessions.get(iter.next());
-						try {
-							session.close();
-						} catch (CVSException e) {
-							CVSProviderPlugin.log(e);
-						}
-					}
-				} finally {
-					currentOpenSessions = null;
-				}
-			}
-		}
-	}
-	
-	/**
-	 * Answer the currently open session
-	 */
-	protected static Session getOpenSession(ICVSResource resource) throws CVSException {
-		ICVSFolder root;
-		if (resource.isFolder()) {
-			root = (ICVSFolder)resource;
-		} else {
-			root = resource.getParent();
-		}
-		// go to the top of the management chain
-		while (root.isManaged()) {
-			root = root.getParent();
-		}
-		// Look for the root in the current open sessions
-		Session session;
-		do {
-			session = (Session)currentOpenSessions.get(root);
-			if (session != null) return session;
-			// we should have found it the first time but continue up the chain
-			root = root.getParent();
-		} while (root != null);
-		return null;
 	}
 	
 	/** 
