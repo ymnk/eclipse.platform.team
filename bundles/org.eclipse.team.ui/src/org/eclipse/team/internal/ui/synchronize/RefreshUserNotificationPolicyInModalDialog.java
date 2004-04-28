e40d67f91824e5b190acce85bc936794946a5f34
Michael Valenta <mvalenta>
2004-04-28 14:52:57 +0000

    *** empty log message ***

--- bundles/org.eclipse.team.ui/src/org/eclipse/team/internal/ui/synchronize/RefreshUserNotificationPolicyInModalDialog.java
+++ bundles/org.eclipse.team.ui/src/org/eclipse/team/internal/ui/synchronize/RefreshUserNotificationPolicyInModalDialog.java
@@ -10,63 +10,70 @@
  *******************************************************************************/
 package org.eclipse.team.internal.ui.synchronize;
 
+import java.lang.reflect.InvocationTargetException;
 import org.eclipse.compare.CompareConfiguration;
 import org.eclipse.compare.CompareUI;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.team.core.synchronize.SyncInfo;
+import org.eclipse.team.core.synchronize.SyncInfoTree;
 import org.eclipse.team.internal.ui.Policy;
 import org.eclipse.team.internal.ui.Utils;
 import org.eclipse.team.ui.TeamUI;
 import org.eclipse.team.ui.synchronize.*;
 import org.eclipse.team.ui.synchronize.subscribers.*;
+import org.eclipse.ui.actions.ActionFactory;
 
 public class RefreshUserNotificationPolicyInModalDialog implements IRefreshSubscriberListener {
 
 	private SubscriberParticipant participant;
-	private ISynchronizePageConfiguration configuration;
+	private boolean rememberInSyncView;
+	private String targetId;
+	private SyncInfoTree syncInfoSet;
 	private Shell shell;
 
-	public RefreshUserNotificationPolicyInModalDialog(Shell shell, ISynchronizePageConfiguration configuration, SubscriberParticipant participant) {
-		this.configuration  = configuration;
+	public RefreshUserNotificationPolicyInModalDialog(Shell shell, String targetId, SubscriberParticipant participant, SyncInfoTree syncInfoSet) {
+		this.targetId = targetId;
 		this.participant = participant;
+		this.syncInfoSet = syncInfoSet;
 		this.shell = shell;
 	}
 
 	public void refreshStarted(IRefreshEvent event) {
 	}
 
-	public Runnable refreshDone(final IRefreshEvent event) {
+	public ActionFactory.IWorkbenchAction refreshDone(final IRefreshEvent event) {
 		//	Ensure that this event was generated for this participant
-		if (event.getSubscriber() != participant.getSubscriber())
+		if (event.getSubscriber() != participant.getSubscriberSyncInfoCollector().getSubscriber())
 			return null;
 		//	 If the event is for a cancelled operation, there's nothing to do
 		int severity = event.getStatus().getSeverity();
 		if(severity == Status.CANCEL || severity == Status.ERROR) 
 			return null;
 		
-		return new Runnable() {
-			public void run() {
-				try {
+		return new WorkbenchAction() {
+			public void run() {		
 					// If there are no changes
-					if (event.getStatus().getCode() == IRefreshEvent.STATUS_NO_CHANGES) {
+					if (!areChanges()) {
 						MessageDialog.openInformation(shell, Policy.bind("OpenComparedDialog.noChangeTitle"), Policy.bind("OpenComparedDialog.noChangesMessage")); //$NON-NLS-1$ //$NON-NLS-2$
 						return;
 					}
-					if (isSingleFileCompare(event.getResources())) {
-						compareAndOpenEditors(event, participant);
-					} else {
-						compareAndOpenDialog(event, participant);
-					}
-				} finally {
-					if (TeamUI.getSynchronizeManager().get(participant.getId(), participant.getSecondaryId()) == null) {
-						participant.dispose();
-					}
+					compareAndOpenDialog(event, participant);
+					setEnabled(false);
+			}
+			public void dispose() {
+				if (TeamUI.getSynchronizeManager().get(participant.getId(), participant.getSecondaryId()) == null) {
+					participant.dispose();
 				}
 			}
 		};
+	}
+
+	private boolean areChanges() {
+		return ! syncInfoSet.isEmpty();
 	}
 
 	protected boolean isSingleFileCompare(IResource[] resources) {
@@ -76,7 +83,7 @@ public class RefreshUserNotificationPolicyInModalDialog implements IRefreshSubsc
 	protected void compareAndOpenEditors(IRefreshEvent event, SubscriberParticipant participant) {
 		IResource[] resources = event.getResources();
 		for (int i = 0; i < resources.length; i++) {
-			SyncInfo info = participant.getSyncInfoSet().getSyncInfo(resources[i]);
+			SyncInfo info = participant.getSubscriberSyncInfoCollector().getSubscriberSyncInfoSet().getSyncInfo(resources[i]);
 			if (info != null) {
 				SyncInfoCompareInput input = new SyncInfoCompareInput(event.getSubscriber().getName(), info);
 				CompareUI.openCompareEditor(input);
@@ -85,9 +92,29 @@ public class RefreshUserNotificationPolicyInModalDialog implements IRefreshSubsc
 	}
 
 	protected void compareAndOpenDialog(final IRefreshEvent event, final SubscriberParticipant participant) {
+		TreeViewerAdvisor advisor = new TreeViewerAdvisor(targetId, null, syncInfoSet);
 		CompareConfiguration cc = new CompareConfiguration();
-		ParticipantPageSaveablePart input = new ParticipantPageSaveablePart(Utils.getShell(null), cc, configuration, participant);
-		ParticipantPageDialog dialog = new ParticipantPageDialog(shell, input, participant);
+		SynchronizeCompareInput input = new SynchronizeCompareInput(cc, advisor) {
+			public String getTitle() {
+				int numChanges = participant.getSubscriberSyncInfoCollector().getSyncInfoTree().size();
+				if (numChanges > 1) {
+					return Policy.bind("OpenComparedDialog.diffViewTitleMany", Integer.toString(numChanges)); //$NON-NLS-1$
+				} else {
+					return Policy.bind("OpenComparedDialog.diffViewTitleOne", Integer.toString(numChanges)); //$NON-NLS-1$
+				}
+			}
+		};
+		try {
+			// model will be built in the background since we know the compare input was 
+			// created with a subscriber participant
+			input.run(new NullProgressMonitor());
+		} catch (InterruptedException e) {
+			Utils.handle(e);
+		} catch (InvocationTargetException e) {
+			Utils.handle(e);
+		}
+		SynchronizeDialog dialog = new SynchronizeDialog(shell, participant.getName(), input);
+		dialog.setSynchronizeParticipant(participant);
 		dialog.setBlockOnOpen(true);
 		dialog.open();
 	}
