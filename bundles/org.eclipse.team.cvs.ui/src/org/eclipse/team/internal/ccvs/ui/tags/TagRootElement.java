4abda86bad64803c35f06903d34108db63f05739
cvs2svn <platform-releng-dev@eclipse.org>
2004-12-02 12:31:50 +0000

    This commit was manufactured by cvs2svn to create branch 'branch_traversals_20040920'.
    Cherrypick from master 2004-12-02 12:31:48 UTC Michael Valenta <mvalenta> 'Bug 78989 Unfriendly message when working disconnected':
        bundles/org.eclipse.team.cvs.core/schema/connectionmethods.exsd
        bundles/org.eclipse.team.cvs.core/src/org/eclipse/team/internal/ccvs/core/CVSCoreFileModificationValidator.java
        bundles/org.eclipse.team.cvs.core/src/org/eclipse/team/internal/ccvs/core/CVSTeamProvider.java
        bundles/org.eclipse.team.cvs.core/src/org/eclipse/team/internal/ccvs/core/client/Add.java
        bundles/org.eclipse.team.cvs.core/src/org/eclipse/team/internal/ccvs/core/client/CommandOutputListener.java
        bundles/org.eclipse.team.cvs.core/src/org/eclipse/team/internal/ccvs/core/client/listeners/AdminKSubstListener.java
        bundles/org.eclipse.team.cvs.core/src/org/eclipse/team/internal/ccvs/core/client/listeners/LogListener.java
        bundles/org.eclipse.team.cvs.core/src/org/eclipse/team/internal/ccvs/core/client/listeners/UpdateListener.java
        bundles/org.eclipse.team.cvs.core/src/org/eclipse/team/internal/ccvs/core/messages.properties
        bundles/org.eclipse.team.cvs.core/src/org/eclipse/team/internal/ccvs/core/util/CVSDateFormatter.java
        bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/tags/BranchPromptDialog.java
        bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/tags/CVSFileElement.java
        bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/tags/CVSFolderElement.java
        bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/tags/FilteredTagList.java
        bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/tags/LocalProjectTagSource.java
        bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/tags/MultiFolderTagSource.java
        bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/tags/SingleFileTagSource.java
        bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/tags/SingleFolderTagSource.java
        bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/tags/TagAsVersionDialog.java
        bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/tags/TagConfigurationDialog.java
        bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/tags/TagContentAssistProcessor.java
        bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/tags/TagElement.java
        bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/tags/TagRefreshButtonArea.java
        bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/tags/TagRootElement.java
        bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/tags/TagSelectionArea.java
        bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/tags/TagSelectionDialog.java
        bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/tags/TagSelectionWizardPage.java
        bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/tags/TagSource.java
        bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/tags/TagSourceResourceAdapter.java
        bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/tags/TagSourceWorkbenchAdapter.java
        bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/wizards/MergeWizard.java
        bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/wizards/MergeWizardPage.java
        tests/org.eclipse.team.tests.cvs.core/src/org/eclipse/team/tests/ccvs/core/EclipseTest.java
        tests/org.eclipse.team.tests.cvs.core/src/org/eclipse/team/tests/ccvs/core/subscriber/CVSChangeSetTests.java
        tests/org.eclipse.team.tests.cvs.core/src/org/eclipse/team/tests/ccvs/core/subscriber/SyncInfoSource.java
        tests/org.eclipse.team.tests.cvs.core/src/org/eclipse/team/tests/ccvs/ui/SynchronizeViewTestAdapter.java

---
+++ bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/tags/TagRootElement.java
@@ -0,0 +1,75 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.team.internal.ccvs.ui.tags;
+
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.team.internal.ccvs.core.CVSTag;
+import org.eclipse.team.internal.ccvs.ui.*;
+import org.eclipse.ui.model.IWorkbenchAdapter;
+
+/**
+ * Workbench model element that contains a list of tags
+ * of the same type (BRANCH, VERSION or DATE).
+ */
+public class TagRootElement implements IWorkbenchAdapter, IAdaptable {
+	private TagSource tagSource;
+	private int typeOfTagRoot;
+    private final Object parent;
+	
+	public TagRootElement(Object parent, TagSource tagSource, int typeOfTagRoot) {
+		this.parent = parent;
+        this.typeOfTagRoot = typeOfTagRoot;
+		this.tagSource = tagSource;
+	}
+	
+	public Object[] getChildren(Object o) {
+		CVSTag[] childTags = tagSource.getTags(typeOfTagRoot);
+		TagElement[] result = new TagElement[childTags.length];
+		for (int i = 0; i < childTags.length; i++) {
+			result[i] = new TagElement(this, childTags[i]);
+		}
+		return result;
+	}
+	public Object getAdapter(Class adapter) {
+		if (adapter == IWorkbenchAdapter.class) return this;
+		return null;
+	}
+	public ImageDescriptor getImageDescriptor(Object object) {
+		if(typeOfTagRoot==CVSTag.BRANCH) {
+			return CVSUIPlugin.getPlugin().getImageDescriptor(ICVSUIConstants.IMG_BRANCHES_CATEGORY);
+		} else if(typeOfTagRoot==CVSTag.DATE){
+			return CVSUIPlugin.getPlugin().getImageDescriptor(ICVSUIConstants.IMG_DATES_CATEGORY);
+		}else {
+			return CVSUIPlugin.getPlugin().getImageDescriptor(ICVSUIConstants.IMG_VERSIONS_CATEGORY);
+		}
+	}
+	public String getLabel(Object o) {
+		if(typeOfTagRoot==CVSTag.BRANCH) {
+			return Policy.bind("MergeWizardEndPage.branches"); //$NON-NLS-1$
+		} else if(typeOfTagRoot==CVSTag.DATE){
+			return Policy.bind("TagRootElement.0"); //$NON-NLS-1$
+		}else {
+			return Policy.bind("VersionsElement.versions"); //$NON-NLS-1$
+		}	
+	}
+	public Object getParent(Object o) {
+		return parent;
+	}
+	/**
+	 * Gets the typeOfTagRoot.
+	 * @return Returns a int
+	 */
+	public int getTypeOfTagRoot() {
+		return typeOfTagRoot;
+	}
+
+}
---
+++ tests/org.eclipse.team.tests.cvs.core/src/org/eclipse/team/tests/ccvs/core/subscriber/SyncInfoSource.java
@@ -0,0 +1,107 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.team.tests.ccvs.core.subscriber;
+
+import java.util.*;
+
+import junit.framework.AssertionFailedError;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.team.core.TeamException;
+import org.eclipse.team.core.subscribers.Subscriber;
+import org.eclipse.team.core.synchronize.SyncInfo;
+import org.eclipse.team.internal.ccvs.core.*;
+
+/**
+ * This class acts as the source for the sync info used by the subscriber tests.
+ * The purpose is to allow the sync info to be obtained directly from the subscriber 
+ * or through the sync set visible in the sync view.
+ */
+public class SyncInfoSource {
+
+	protected static IProgressMonitor DEFAULT_MONITOR = new NullProgressMonitor();
+	protected List mergeSubscribers = new ArrayList();
+	protected List compareSubscribers = new ArrayList();
+	
+	public CVSMergeSubscriber createMergeSubscriber(IProject project, CVSTag root, CVSTag branch) {
+		CVSMergeSubscriber subscriber = new CVSMergeSubscriber(new IResource[] { project }, root, branch);
+		mergeSubscribers.add(subscriber);
+		return subscriber;
+	}
+	
+	public CVSCompareSubscriber createCompareSubscriber(IResource resource, CVSTag tag) {
+		CVSCompareSubscriber subscriber = new CVSCompareSubscriber(new IResource[] { resource }, tag);
+		compareSubscribers.add(subscriber);
+		return subscriber;
+	}
+	
+	public Subscriber createWorkspaceSubscriber() throws TeamException {
+		return CVSProviderPlugin.getPlugin().getCVSWorkspaceSubscriber();
+	}
+	
+	/**
+	 * Return the sync info for the given subscriber for the given resource.
+	 */
+	public SyncInfo getSyncInfo(Subscriber subscriber, IResource resource) throws TeamException {
+		return subscriber.getSyncInfo(resource);
+	}
+	
+	/**
+	 * Refresh the subscriber for the given resource
+	 */
+	public void refresh(Subscriber subscriber, IResource resource) throws TeamException {
+		refresh(subscriber, new IResource[] { resource});
+	}
+	
+	/**
+	 * Refresh the subscriber for the given resources
+	 */
+    public void refresh(Subscriber subscriber, IResource[] resources) throws TeamException {
+        subscriber.refresh(resources, IResource.DEPTH_INFINITE, DEFAULT_MONITOR);
+    }
+    
+	protected void assertProjectRemoved(Subscriber subscriber, IProject project) throws TeamException {
+		IResource[] roots = subscriber.roots();
+		for (int i = 0; i < roots.length; i++) {
+			IResource resource = roots[i];
+			if (resource.equals(project)) {
+				throw new AssertionFailedError();
+			}
+		}
+	}
+
+	public void tearDown() {
+		for (Iterator it = mergeSubscribers.iterator(); it.hasNext(); ) {
+			CVSMergeSubscriber s = (CVSMergeSubscriber) it.next();
+			s.cancel();
+		}
+	}
+
+	/**
+	 * Recalculate a sync info from scratch
+	 */
+	public void reset(Subscriber subscriber) throws TeamException {
+		// Do nothing
+		
+	}
+	
+	/**
+	 * Assert that the model for the subscriber matches what is being displayed.
+	 * Default is to do nothing. Subclasses may override
+	 * @param subscriber the subscriber
+	 */
+	public void assertViewMatchesModel(Subscriber subscriber) {
+	    // Default is to do nothing. Subclasses may override
+	}
+}
