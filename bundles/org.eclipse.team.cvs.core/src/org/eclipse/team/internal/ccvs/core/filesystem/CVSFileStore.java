cda64b94cdd65b7621a258cc3b967b1c5410fe78
Michael Valenta <mvalenta>
2005-10-03 16:17:40 +0000

    More work on CVS scheme for EFS

--- bundles/org.eclipse.team.cvs.core/src/org/eclipse/team/internal/ccvs/core/filesystem/CVSFileStore.java
+++ bundles/org.eclipse.team.cvs.core/src/org/eclipse/team/internal/ccvs/core/filesystem/CVSFileStore.java
@@ -1,60 +1,137 @@
+/**********************************************************************
+ * Copyright (c) 2005 IBM Corporation and others. All rights reserved.   This
+ * program and the accompanying materials are made available under the terms of
+ * the Common Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors: 
+ * IBM - Initial API and implementation
+ **********************************************************************/
 package org.eclipse.team.internal.ccvs.core.filesystem;
 
 import java.io.InputStream;
 import java.net.URI;
 
+import org.eclipse.core.filesystem.FileSystemCore;
 import org.eclipse.core.filesystem.IFileInfo;
 import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.core.filesystem.IFileStoreConstants;
+import org.eclipse.core.filesystem.provider.FileInfo;
 import org.eclipse.core.filesystem.provider.FileStore;
+import org.eclipse.core.resources.IStorage;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.team.core.TeamException;
+import org.eclipse.team.core.variants.IResourceVariant;
+import org.eclipse.team.internal.ccvs.core.CVSProviderPlugin;
+import org.eclipse.team.internal.ccvs.core.ICVSRemoteFile;
+import org.eclipse.team.internal.ccvs.core.ICVSRemoteFolder;
 import org.eclipse.team.internal.ccvs.core.ICVSRemoteResource;
+import org.eclipse.team.internal.ccvs.core.ICVSResource;
+import org.eclipse.team.internal.ccvs.core.ILogEntry;
 
 public class CVSFileStore extends FileStore {
 
-	private final ICVSRemoteResource remoteResource;
+	private final CVSURI uri;
+	private IFileInfo info;
 
-	public CVSFileStore(ICVSRemoteResource remoteResource) {
-		this.remoteResource = remoteResource;
+	public CVSFileStore(CVSURI uri, IFileInfo info) {
+		this.uri = uri;
+		this.info = info;
 	}
 
 	public String[] childNames(int options, IProgressMonitor monitor) {
-		// TODO Auto-generated method stub
-		return null;
+		IFileInfo[] infos = childInfos(options, monitor);
+		String[] names = new String[infos.length];
+		for (int i = 0; i < infos.length; i++) {
+			names[i] = infos[i].getName();
+		}
+		return names;
+	}
+
+	public IFileStore[] childStores(int options, IProgressMonitor monitor) {
+		IFileInfo[] infos = childInfos(options, monitor);
+		IFileStore[] children = new IFileStore[infos.length];
+		for (int i = 0; i < infos.length; i++) {
+			children[i] = getChild(infos[i]);
+		}
+		return children;
+	}
+	
+	private IFileStore getChild(IFileInfo info) {
+		return new CVSFileStore(uri.append(info.getName()), info);
 	}
 
 	public IFileInfo fetchInfo(int options, IProgressMonitor monitor) {
-		// TODO Auto-generated method stub
-		return null;
+		ICVSRemoteFolder folder = uri.getParentFolder();
+		if (folder == null) {
+			// this is the repo root so return an info that indicates this
+			FileInfo info = new FileInfo();
+			info.setExists(true);
+			info.setName(uri.getRepositoryName());
+			info.setAttribute(IFileStoreConstants.ATTRIBUTE_DIRECTORY, true);
+		}
+		try {
+			ICVSResource[] children = folder.fetchChildren(monitor);
+			ICVSResource resource = null;
+			for (int i = 0; i < children.length; i++) {
+				ICVSResource child = children[i];
+				if (child.getName().equals(getName())) {
+					resource = child;
+					break;
+				}
+			}
+			return getFileInfo(resource, monitor);
+		} catch (CoreException e) {
+			// TODO Need to handle this somehow
+			CVSProviderPlugin.log(e);
+			return null;
+		}
+	}
+
+	private IFileInfo getFileInfo(ICVSResource resource, IProgressMonitor monitor) throws TeamException {
+		if (resource == null)
+			return null;
+		FileInfo info = new FileInfo();
+		info.setExists(true);
+		info.setName(resource.getName());
+		info.setAttribute(IFileStoreConstants.ATTRIBUTE_DIRECTORY, resource.isFolder());
+		if (!resource.isFolder()) {
+			ICVSRemoteFile file = (ICVSRemoteFile)resource;
+			ILogEntry entry = file.getLogEntry(monitor);
+			info.setLastModified(entry.getDate().getTime());
+		} else {
+			info.setLastModified(0);
+		}
+		return info;
 	}
 
 	public IFileStore getChild(String name) {
-		// TODO Auto-generated method stub
-		return null;
+		if (info != null && !info.isDirectory()) {
+			return null;
+		}
+		return new CVSFileStore(uri.append(name), null);
 	}
 
 	public String getName() {
-		return remoteResource.getName();
+		return uri.getLastSegment();
 	}
 
 	public IFileStore getParent() {
-		// TODO Remote resource may not have a parent
-		return new CVSFileStore((ICVSRemoteResource)remoteResource.getParent());
+		if (uri.isRepositoryRoot()) {
+			return null;
+		}
+		return new CVSFileStore(uri.removeLastSegment(), null);
 	}
 
-	public IFileStore mkdir(int options, IProgressMonitor monitor) throws CoreException {
-		throw new UnsupportedOperationException();
-	}
-
-	public InputStream openInputStream(int options, IProgressMonitor monitor)
-			throws CoreException {
-		// TODO Auto-generated method stub
-		return null;
+	public InputStream openInputStream(int options, IProgressMonitor monitor) throws CoreException {
+		ICVSRemoteFile file = uri.toFile();
+		IStorage storage = ((IResourceVariant)file).getStorage(monitor);
+		return storage.getContents();
 	}
 
 	public URI toURI() {
-		// TODO Auto-generated method stub
-		return null;
+		return uri.toURI();
 	}
 
 }
--- bundles/org.eclipse.team.cvs.core/src/org/eclipse/team/internal/ccvs/core/filesystem/CVSFileSystem.java
+++ bundles/org.eclipse.team.cvs.core/src/org/eclipse/team/internal/ccvs/core/filesystem/CVSFileSystem.java
@@ -1,15 +1,17 @@
+/**********************************************************************
+ * Copyright (c) 2005 IBM Corporation and others. All rights reserved.   This
+ * program and the accompanying materials are made available under the terms of
+ * the Common Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors: 
+ * IBM - Initial API and implementation
+ **********************************************************************/
 package org.eclipse.team.internal.ccvs.core.filesystem;
-
 import java.net.URI;
 
 import org.eclipse.core.filesystem.IFileStore;
 import org.eclipse.core.filesystem.provider.FileSystem;
-import org.eclipse.osgi.util.NLS;
-import org.eclipse.team.internal.ccvs.core.CVSTag;
-import org.eclipse.team.internal.ccvs.core.ICVSRemoteFolder;
-import org.eclipse.team.internal.ccvs.core.ICVSRemoteResource;
-import org.eclipse.team.internal.ccvs.core.ICVSRepositoryLocation;
-import org.eclipse.team.internal.ccvs.core.resources.RemoteFolder;
 
 public class CVSFileSystem extends FileSystem {
 
@@ -18,33 +20,7 @@ public class CVSFileSystem extends FileSystem {
 	}
 
 	public IFileStore getStore(URI uri) {
-		return new CVSFileStore(getRemoteResource(uri));
-	}
-
-	private ICVSRemoteResource getRemoteResource(URI uri) {
-		ICVSRepositoryLocation repository = getRepository(uri);
-		if (repository == null)
-			throw new IllegalArgumentException(NLS.bind("Invalid uri {0}", new String[] {uri.toString()}));
-		String path = getPath(uri);
-		CVSTag tag = getTag(uri);
-		ICVSRemoteFolder folder = new RemoteFolder(null, repository, path, tag);
-		return null;
-	}
-
-	private CVSTag getTag(URI uri) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	private String getPath(URI uri) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	private ICVSRepositoryLocation getRepository(URI uri) {
-		String host = uri.getHost();
-		
-		return null;
+		return new CVSFileStore(CVSURI.fromUri(uri), null);
 	}
 
 }
