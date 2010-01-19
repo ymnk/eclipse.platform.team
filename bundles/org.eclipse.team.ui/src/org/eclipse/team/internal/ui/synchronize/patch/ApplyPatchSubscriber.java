96ca93ff2a12ee7622e60aa66238c7a73d4f9fcc
Tomasz Zarna <tzarna>
2010-01-19 18:06:28 +0000

    bug 298923: [Sync View][Apply patch] Merge changes from dev branch back to HEAD -- Ongoing work, comment 16

--- bundles/org.eclipse.team.ui/src/org/eclipse/team/internal/ui/synchronize/patch/ApplyPatchSubscriber.java
+++ bundles/org.eclipse.team.ui/src/org/eclipse/team/internal/ui/synchronize/patch/ApplyPatchSubscriber.java
@@ -29,6 +29,24 @@ import org.eclipse.team.internal.core.mapping.LocalResourceVariant;
 
 public class ApplyPatchSubscriber extends Subscriber {
 	
+	private class ApplyPatchSyncInfo extends SyncInfo {
+		private ApplyPatchSyncInfo(IResource local, IResourceVariant base,
+				IResourceVariant remote, IResourceVariantComparator comparator) {
+			super(local, base, remote, comparator);
+		}
+
+		protected int calculateKind() throws TeamException {
+			// TODO: this works only for files, what about excluding individual hunks?
+			if (!getPatcher().isEnabled(PatchModelProvider.getPatchObject(getLocal(), patcher)))
+				return IN_SYNC;
+			// mark diffs with problems as conflicts 
+			if (getRemote() != null 
+					&& getPatcher().getDiffResult(((PatchedFileVariant)getRemote()).getDiff()).containsProblems())
+				return CONFLICTING;
+			return super.calculateKind();
+		}
+	}
+
 	private WorkspacePatcher patcher;
 	private IResourceVariantComparator comparator;
 
@@ -47,46 +65,35 @@ public class ApplyPatchSubscriber extends Subscriber {
 	}
 
 	public SyncInfo getSyncInfo(IResource resource) throws TeamException {
-		FilePatch2[] diffs = getPatcher().getDiffs();
+		if (!isSupervised(resource)) return null;
+		// XXX: doing this here is highly inefficient!
+		// getPatcher().refresh();
+		// a little bit better but still called gazzilon times
+		refresh(new IResource[] { resource }, IResource.DEPTH_ZERO, null);
 		try {
+			FilePatch2 diff = (FilePatch2) PatchModelProvider.getPatchObject(resource, getPatcher());
+			// use null as remote variant for deletions
 			IResourceVariant variant = null;
-			if (resource.getType() == IResource.FILE) {
-				for (int i = 0; i < diffs.length; i++) {
-					if (resource.equals(PatchModelProvider.getFile(diffs[i], getPatcher()))) {
-						// null as 'variant' for deletions
-						if (diffs[i].getDiffType(patcher.isReversed()) != FilePatch2.DELETION)
-							variant =  new PatchedFileVariant(getPatcher(), diffs[i]);
-						IResourceVariant base = resource.exists() ?  new LocalResourceVariant(resource) : null;
-						SyncInfo info = new SyncInfo(resource, base, variant, getResourceComparator()) {
-							protected int calculateKind() throws TeamException {
-								// TODO: this will work only for files, what about excluding individual hunks?
-								if (!getPatcher().isEnabled(PatchModelProvider.getPatchObject(getLocal(), patcher)))
-									return IN_SYNC;
-								if (getRemote() != null 
-										&& getPatcher().getDiffResult(((PatchedFileVariant)getRemote()).getDiff()).containsProblems())
-									return CONFLICTING;
-								return super.calculateKind();
-							}
-						};
-						info.init();
-						return info;
-					}
-				}
-			}
-			return null;
+			if (diff.getDiffType(patcher.isReversed()) != FilePatch2.DELETION)
+				variant =  new PatchedFileVariant(getPatcher(), diff);
+			IResourceVariant base = resource.exists() ?  new LocalResourceVariant(resource) : null;
+			SyncInfo info = new ApplyPatchSyncInfo(resource, base, variant, getResourceComparator());
+			info.init();
+			return info;
 		} catch (CoreException e) {
 			throw TeamException.asTeamException(e);
 		}
 	}
 
 	public boolean isSupervised(IResource resource) throws TeamException {
-		return true;
+		return resource.getType() == IResource.FILE
+				&& PatchModelProvider.getPatchObject(resource, getPatcher()) != null;
 	}
 
 	public IResource[] members(IResource resource) throws TeamException {
 		try {
 			if(resource.getType() == IResource.FILE)
-				// file has no members
+				// file has no IResource members
 				return new IResource[0];
 			IContainer container = (IContainer) resource;
 
@@ -109,8 +116,16 @@ public class ApplyPatchSubscriber extends Subscriber {
 
 	public void refresh(IResource[] resources, int depth,
 			IProgressMonitor monitor) throws TeamException {
-		// TODO: refresh 'resources' only, not the whole patch 
-		getPatcher().refresh();
+		Set /* <FilePatch> */diffs = new HashSet();
+		for (int i = 0; i < resources.length; i++) {
+			Object object = PatchModelProvider.getPatchObject(resources[i],
+					getPatcher());
+			if (object instanceof FilePatch2) {
+				FilePatch2 filePatch = (FilePatch2) object;
+				diffs.add(filePatch);
+			}
+		}
+		getPatcher().refresh((FilePatch2[]) diffs.toArray(new FilePatch2[0]));
 	}
 
 	public IResource[] roots() {
